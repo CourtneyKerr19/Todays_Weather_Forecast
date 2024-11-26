7f330e2dcaf8b9bfba9aa96e5748157a5e403cb5
Courtney Kerr <courtneykerr219@gmail.com>
2024-11-26 08:36:03 -0600

    Add a final showImage function that will basically allow the image to be displayed on the web page.

--- script.js
+++ script.js
@@ -94,3 +94,8 @@ function displayHourlyForecast(hourlyData) {
     hourlyForecastDiv.innerHTML += hourlyItemHtml;
   });
 }
+
+function showImage() {
+  const weatherIcon = document.getElementById('weather-icon');
+  weatherIcon.style.display = 'block';
+}
