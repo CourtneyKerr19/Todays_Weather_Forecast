22e1170c6fe2a6b77fb4a256146f8b67bf9a02dd
Courtney Kerr <courtneykerr219@gmail.com>
2024-11-26 07:28:42 -0600

    Added an error message in the instance that the user input is empty.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getWeather() {
   const apiKey = "cea1b5c39f90da85436bec567502aeb5";
   const city = document.getElementById("city").value;
+
+  if (!city) {
+    alert("Please enter a city name.");
+    return;
+  }
 }
