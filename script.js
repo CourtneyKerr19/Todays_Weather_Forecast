abd2087b082f2c150686b2329e49c24ea4f7c6d3
Courtney Kerr <courtneykerr219@gmail.com>
2024-11-26 08:24:32 -0600

    Adding some more HTML content for the data provided.

--- script.js
+++ script.js
@@ -78,6 +78,19 @@ function displayHourlyForecast(hourlyData) {
   const next24Hours = hourlyData.slice(0, 8);
 
   next24Hours.forEach(item => {
+    const dateTime = new Date(item.dt * 1000);
+    const hour = dateTime.getHours();
+    const temperature = Math.round(item.main.temp - 273.15);
+    const iconCode = item.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
 
+    const hourlyItemHtml = `
+      <div class="hourly-item">
+        <span>${hour}:00</span>
+        <img src="${iconUrl}" alt="Hourly Weather Icon">
+        <span>${temperature}°C</span>
+      </div>
+    `;
+    hourlyForecastDiv.innerHTML += hourlyItemHtml;
   });
 }
