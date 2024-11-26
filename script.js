e637cff1e285b9400cf23706bd54ff8a4feb806e
Courtney Kerr <courtneykerr219@gmail.com>
2024-11-26 07:57:54 -0600

    Implemented fetch function to fetch the currentWeatherUrl using json and display the weather data. As well as implementing error messages in the instance that there is an issue fetching the weather data.

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function getWeather() {
 
 const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 const forecaseUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+function getWeather() {
+  fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+      displayWeather(data);
+    })
+    .catch(error => {
+      console.error('Error fetching current weather data in your area:', error);
+      alert('Error fetching current weather data. Please try again.')
+    })
+}
